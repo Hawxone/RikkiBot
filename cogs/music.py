9d48fa21b065dc6dfebbdeab2550a702c6da303b
Hawxone <hawxseven@gmail.com>
2019-03-09 16:29:44 +0700

    changed youtube-dl ignoreerrors to TRUE
    # Conflicts:
    #	bot.py
    #	cogs/ai.py
    #	cogs/commands.py
    #	cogs/music.py
    #	db.sqlite3
    #	requirements.txt

--- cogs/music.py
+++ cogs/music.py
@@ -24,7 +24,7 @@ ytdlopts = {
     'restrictfilenames': True,
     'noplaylist': False,
     'nocheckcertificate': True,
-    '--ignore-errors': True,
+    'ignoreerrors': True,
     'logtostderr': False,
     'quiet': True,
     'no_warnings': True,
